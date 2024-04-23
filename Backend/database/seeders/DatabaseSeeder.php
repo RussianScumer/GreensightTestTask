6e87ece38e8e9b1e4bfbe143c07428505510b6f7
Korneevec Ilia <korneevecin@mail.ru>
2024-04-24 01:40:02 +0300

    add api routes

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // UserController::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Test UserController',
             'email' => 'test@example.com',
         ]);
     }
